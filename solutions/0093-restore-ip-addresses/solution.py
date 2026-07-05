76271ae5d2d8ed56209bb56b6a9a36f6b225c316
Nitish Pattar <nitishpattar7@gmail.com>
2026-07-05 10:30:41 +0000

    Sync LeetCode submission Runtime - 4 ms (26.18%), Memory - 19.5 MB (8.07%)

---
+++ solutions/0093-restore-ip-addresses/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        def dfs(i, cur, dots):
+            if dots == 4 and i == len(s): res.append(cur[:-1]); return
+            if dots > 4: return
+            for j in range(i, min(i + 3, len(s))):
+                if int(s[i:j+1]) < 256 and (i == j or s[i] != '0'):
+                    dfs(j + 1, cur + s[i:j+1] + ".", dots + 1)
+        dfs(0, "", 0)
+        return res
+
